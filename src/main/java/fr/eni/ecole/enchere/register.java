3222b724325fd504ae54242cd64c9f9168a5fec6
Fugann <95033152+Fugann@users.noreply.github.com>
2023-12-04 18:15:36 +0100

    push

--- src/main/java/fr/eni/ecole/enchere/register.java
+++ src/main/java/fr/eni/ecole/enchere/register.java
@@ -15,20 +15,24 @@ public class register extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/register.jsp");
 		rd.forward(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/register.jsp");
-		
+
 		String pseudo = request.getParameter("pseudo");
 		String prenom = request.getParameter("prenom");
 		String tel = request.getParameter("tel");
@@ -39,22 +43,15 @@ public class register extends HttpServlet {
 		String rue = request.getParameter("rue");
 		String ville = request.getParameter("ville");
 		String pswconfirm = request.getParameter("pswconfirm");
-		
-	 if (psw == null || !psw.equals(pswconfirm)) {
-            request.setAttribute("error", "Le mot de passe ne correspond pas !");
-        } else if (
-                pseudo == null || pseudo.equals("") ||
-                prenom == null || prenom.equals("") ||
-                CP == null || CP.equals("") ||
-                psw == null || psw.equals("") ||
-                nom == null || nom.equals("") ||
-                email == null || email.equals("") ||
-                rue == null || rue.equals("") ||
-                ville == null || ville.equals("")) {
-			
-            request.setAttribute("error", "Veuillez remplir tous les champs !");
-        }
-		
+
+		if (psw == null || !psw.equals(pswconfirm)) {
+			request.setAttribute("error", "Le mot de passe ne correspond pas !");
+		} else if (pseudo == null || pseudo.equals("") || prenom == null || prenom.equals("") || CP == null
+				|| CP.equals("") || psw == null || psw.equals("") || nom == null || nom.equals("") || email == null
+				|| email.equals("") || rue == null || rue.equals("") || ville == null || ville.equals("")) {
+			request.setAttribute("error", "Veuillez remplir tous les champs !");
+		}
+
 		rd.forward(request, response);
 
 	}
--- src/main/webapp/WEB-INF/views/register.jsp
+++ src/main/webapp/WEB-INF/views/register.jsp
@@ -1,7 +1,7 @@
 <%@ page language="java"
 	contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -13,9 +13,6 @@
 <body>
 
 <p>Mon profil</p>
-
-
-
 
 <form method="post" action="">
 	<div>
